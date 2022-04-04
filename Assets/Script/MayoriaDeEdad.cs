14061b36cf528e526ae23d8de3c7746832a9eda6
CaroKaufman <101335676+CaroKaufman@users.noreply.github.com>
2022-04-04 12:10:00 -0300

    TP2 UI

---
+++ Assets/Script/MayoriaDeEdad.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayoriaDeEdad : MonoBehaviour
+{
+    public int EdadUsuario;
+    public Text miTexto;
+    void Start()
+    {
+        
+        if (EdadUsuario>=18)
+        {
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            Debug.Log("No es mayor de edad");
+        }
+    }
+
+   
+}
